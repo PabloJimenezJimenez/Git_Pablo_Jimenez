7ee528b7447dbaa986a8da641cbd2281a527f585
Pablo Jimenez <Pablo.jimenez2@live.u-tad.com>
2022-12-21 12:43:26 +0100

    Modificacion 3

--- Git_Pablo_Jimenez/src/PJ.java
+++ Git_Pablo_Jimenez/src/PJ.java
@@ -3,7 +3,7 @@ public class PJ {
 
 	public static void main(String[] args) {
 		System.out.println("Alumno: PabloJimenez");
-		System.out.println("Primera Modificación desde Master");
+		System.out.println("Modificación desde Master - Cambiado");
 	}
 
 }
